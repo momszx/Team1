5e4cb2f7a3d1e9bc0138d4dcb67cd37a261a45c6
norri04 <norri04@users.noreply.github.com>
2020-03-20 19:11:14 +0100

    Game loop, Timer, Graphics,BufferStrategies, Superclasses
    - Első karakter képe eltávolítva
    - Tile / block és Entity osztályok létrehozva, alap dolgok megírva(superclass-ok)
    - Game-ben start(), stop(), run() és render() metódusok megírva, tick() üres törzzsel rendelkezik még. A játékablak színnel van kitöltve még.
    
    ##
    ez helyett: languageLevel="JDK_11"
    ez lett: languageLevel="JDK_13"
    
    ez a verzió, i guess.. kiabált miatta úgyhogy átállítottam, ha nektek így jó akkor oké, egyébként ha van tippetek hogy nekemmiért nem jó 11-essel akkor nyugodtan szóljatok
    ##

--- 2DGame/src/com/company/Game.java
+++ 2DGame/src/com/company/Game.java
@@ -2,9 +2,10 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferStrategy;
 import javax.swing.JFrame;
 
-public class Game extends Canvas {
+public class Game extends Canvas  implements Runnable{
 
     public static  final int WIDTH =200;
     public static  final int HEIGHT = WIDTH/14*10;
@@ -18,15 +19,82 @@ public class Game extends Canvas {
     setMaximumSize(size);
     setMinimumSize(size);
     }
+    private Thread thread;
+    private boolean running = false;
+
+    private synchronized void start(){
+        if(running) return;
+        running = true;
+        thread = new Thread(this,"Thread"); //Thread will be the name of this thread.
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        long lastTime = System.nanoTime(); //current time in nanoseconds
+        long timer = System.currentTimeMillis(); //current time in milliseconds
+        double delta = 0.0;
+        double ns = 1000000000.0/60.0; //nanosecond
+        int frames = 0;
+        int ticks = 0;
+        while(running){
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while(delta>=1){
+                tick();
+                ticks++;
+                delta--;
+            }
+            render();
+            frames++;
+            if(System.currentTimeMillis()-timer > 1000){
+                timer += 1000;
+                System.out.println(frames + "frames per seconds, " + ticks + " update per seconds.");
+                frames = 0;
+                ticks = 0;
+            }
+        }
+        stop();
+    }
+
+    private synchronized void stop(){
+        if(!running) return;
+        running = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void render(){ //displaying on the screen
+        BufferStrategy bs = getBufferStrategy();
+        if(bs==null){
+            createBufferStrategy(3); //3 bufferstrategy will build up our screen
+            return;
+        }
+        Graphics g = bs.getDrawGraphics();
+        g.setColor(Color.MAGENTA);
+        g.fillRect(0,0,getWidth(), getHeight());
+        g.dispose();
+        bs.show();
+    }
+
+    public void tick(){ //update
+
+    }
 
     public static void main (String[] args){
         Game game = new Game();
-        JFrame frame = new Frame(TITTLE);
+        JFrame frame = new JFrame(TITTLE);
         frame.add(game);
         frame.pack();
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+        game.start();
     }
+
 }
---
+++ 2DGame/src/com/company/entity/Entity.java
@@ -0,0 +1,62 @@
+package com.company.entity;
+
+import java.awt.*;
+
+public class Entity {
+    public int x;
+    public int y;
+    public int width, height;
+    public boolean solid;
+
+    public int velX; //velocity
+    public int velY;
+
+    public Entity(int x, int y, int width, int height, boolean solid){
+        this.setX(x);
+        this.setY(y);
+        this.width=width;
+        this.height=height;
+        this.setSolid(solid);
+    }
+
+    public void render(Graphics g){
+
+    }
+    public void tick(){
+        x+=velX;
+        y+=velY;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public boolean isSolid() {
+        return solid;
+    }
+
+    public void setSolid(boolean solid) {
+        this.solid = solid;
+    }
+
+
+    public void setVelX(int velX) {
+        this.velX = velX;
+    }
+
+    public void setVelY(int velY) {
+        this.velY = velY;
+    }
+}
